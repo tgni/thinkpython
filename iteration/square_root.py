bc85266fe6f32e9ae1d62df569a2ad34c3bcb36d
tgni <nitonggui@aliyun.com>
2017-12-01 21:29:10 +0800

    square root

---
+++ iteration/square_root.py
@@ -0,0 +1,14 @@
+from math import *
+
+def square_root(a, x):
+    while True:
+        print x
+        y = (x + a/x) / 2
+        if abs(y-x) < 0.00000001:
+            return y
+        x = y
+
+a = float(raw_input('Pls input a number:'))
+x = float(raw_input('Pls input a x for iteration:'))
+
+print 'Square of ' + str(a) + 'is ' + str(square_root(a, x))
