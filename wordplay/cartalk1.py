ebdbf682081e0eda2000771d99573e7eb3ef236e
tgni <nitonggui@aliyun.com>
2017-12-04 22:01:45 +0800

    cartalk 1&2

---
+++ wordplay/cartalk1.py
@@ -0,0 +1,21 @@
+""" find consecutive three double letter word, eg: bananana
+"""
+def find_seq(word):
+    i     = 0
+    count = 0
+    while i < len(word) - 1:
+        if word[i] == word[i+1]:
+            count = count + 1
+            if count == 3:
+                return True
+            i = i + 2
+        else:
+            count = 0
+            i = i + 1
+    return False
+
+fin = open('words.txt')
+for line in fin:
+    word = line.strip()
+    if find_seq(word):
+        print word
---
+++ wordplay/cartalk2.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    return s[::-1] == s
+
+n = 100000
+max = 999999
+
+while n < max:
+    if is_palindrome(str(n)):
+        print n
+    n = n+1
+
