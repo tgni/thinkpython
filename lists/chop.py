94eb836f258db7739d50b4928de55d78dbcc952c
tgni <nitonggui@aliyun.com>
2017-12-06 13:41:00 +0800

    lists

---
+++ lists/chop.py
@@ -0,0 +1,8 @@
+def chop(t):
+    del t[0]
+    del t[-1]
+
+t = ['a', 'b', 'c', 'd']
+t1 = chop(t)
+print t
+print t1
---
+++ lists/sum_list.py
@@ -0,0 +1,13 @@
+def sum_list(t):
+    res = []
+    sum = 0
+    for s in t:
+        sum += s
+        res.append(sum)
+    return res
+
+t = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+t1 = sum_list(t)
+
+print t
+print t1
