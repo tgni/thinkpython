ec29002ce707cb0130ad4677873026260a3e1d43
tgni <nitonggui@aliyun.com>
2017-12-06 21:49:51 +0800

    lists

---
+++ lists/has_duplicates.py
@@ -0,0 +1,9 @@
+def has_duplicates(t):
+    for i in range(len(t)):
+        for s in t[i+1:]:
+            if t[i] == s:
+                return True
+    return False
+
+t = ['a', 'e', 'c', 'd', 'e', 't', 's']
+print t, has_duplicates(t)
---
+++ lists/remove_duplicates.py
@@ -0,0 +1,14 @@
+def remove_duplicates(t):
+    res = []
+    for e in t:
+        for s in res:
+            if e == s:
+                break 
+        else:
+            res.append(e)
+    return res
+
+t = ['a', 'e', 'e', 'f', 's', 't', 's']
+print t
+s = remove_duplicates(t)
+print s
