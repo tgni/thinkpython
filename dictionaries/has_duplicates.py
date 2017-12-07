ae713b846bf66b984962ae756c7b18d322bd7b85
tgni <nitonggui@aliyun.com>
2017-12-07 16:30:38 +0800

    rotate

---
+++ dictionaries/has_duplicates.py
@@ -0,0 +1,13 @@
+def has_duplicates(t):
+    d = {}
+
+    for x in t:
+        if x in d:
+            return True
+        d[x] = True
+
+def has_duplicates2(t):
+    return len(set(t)) < len(t)
+
+t = ['a', 'e', 'c', 'd', 'e', 't', 's']
+print t, has_duplicates(t)
