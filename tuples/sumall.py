ef6de4559fa76b2a6fbcfe8ee1a27dcffb9acc38
tgni <nitonggui@aliyun.com>
2017-12-08 17:04:36 +0800

    tuples

---
+++ tuples/sumall.py
@@ -0,0 +1,5 @@
+def sumall(*args):
+    print args
+    return sum(args)
+
+print sumall(1, 2, 3, 4, 5, 6)
---
+++ tuples/unstable_sort.py
@@ -0,0 +1,29 @@
+from random import *
+
+def sort_by_length(words):
+    t = []
+    for word in words:
+        t.append((len(word), word))
+        t.sort(reverse=True)
+    res = []
+    for length, word in t:
+        res.append(word)
+    return res
+
+def sort_by_length_random(words):
+    t = []
+    for word in words:
+        t.append((len(word), 100*random(), word))
+        t.sort(reverse=True)
+    res = []
+    for length, rand, word in t:
+        res.append(word)
+    return res
+
+if __name__ == '__main__':
+    t = ['abc', 'aa', 'cc', 'deb', 'adfwe', 'fewe', 'dafef', 'adsfwefad', 'dfefe']
+    s  = sort_by_length(t)
+    s1 = sort_by_length_random(t)
+    print t
+    print s
+    print s1
