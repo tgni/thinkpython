3e0897736784416ded869ae748a6707b49c640f3
tgni <nitonggui@gmail.com>
2024-11-12 14:40:23 +0800

    delete liaoxuefeng

--- liaoxuefeng/list_tuple/print_apple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-L = [
-    ['Apple', 'Google', 'Microsoft'],
-    ['Java', 'Python', 'Ruby', 'PHP'],
-    ['Adam', 'Bart', 'Bob']
-]
-
-print(L[0][0], L[1][1], L[2][2])
